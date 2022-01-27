dfbe0bfcdd89cc57a6e6849cf8d70828919200e7
Ivan <ivan.morozov.nrj@yandex.ru>
2022-01-27 23:39:15 +0300

    2n practice

--- script/j.js
+++ script/j.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотери?");
 
 
@@ -9,12 +11,31 @@ let personalMovieDB = {
     genres:[],
     privat: false
 };
-const a = prompt ("Какой последний фильм?"),
-      b = prompt ("Как его оцените?"),
-      c = prompt ("Какой последний фильм?"),
-      d = prompt ("Как его оцените?");
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt ("Какой последний фильм?"),
+          b = prompt ("Как его оцените?");
+          personalMovieDB.movies [a] = b;
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50 ) {
+        personalMovieDB.movies [a] = b;
+    
+    } else {
+     i--;
+    }
+}
+
+
+ if (personalMovieDB.count < 10) {
+     alert("Просмотрено довольно мало фильмов");
+ } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+     alert("Вы классический зритель");
+ } else if (personalMovieDB.count > 30) {
+     alert("Вы киноман");
+ } else {
+     alert("Произошла ошибка");
+ }
+
 
 console.log (personalMovieDB);
